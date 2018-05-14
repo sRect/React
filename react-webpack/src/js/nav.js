e96721f6512612268aa31d24f4536658354a24a5
srect <fcqun@sina.cn>
2018-05-14 14:43:41 +0800

    NavLink增加样式

--- react-webpack/src/js/nav.js
+++ react-webpack/src/js/nav.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
 
+import "../css/nav.css"
+
 const NavBar = () => {
   return(
     <div>
-    <NavLink exact to="/">PageA</NavLink> | &nbsp;
-    <NavLink exact to="pageb">PageB</NavLink>
+    <NavLink exact to="/" className="blue" activeClassName="active">PageA</NavLink> | &nbsp;
+    <NavLink exact to="/pageb" activeClassName="active">PageB</NavLink>
   </div>
   ) 
 }
