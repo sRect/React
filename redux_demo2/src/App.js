b4469c8b7095d80caa0a45d04f5f414a560c395d
srect <fcqun@sina.cn>
2018-07-30 18:02:58 +0800

    :art: 使用react-redux

--- redux_demo2/src/App.js
+++ redux_demo2/src/App.js
@@ -1,41 +1,16 @@
 import React, { Component } from 'react'
-import { createStore } from 'redux'
-
-// 定义reducer(计算规则)
-let reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INCREASE":
-      return state + 1;
-    case "DECREASE":
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-// 根据计算规则，生成store
-let store = createStore(reducer)
-
-// 派发规则
-store.subscribe(() => {
-  console.log(`current state:${store.getState()}`)
-})
-
-// 触发数据变化
-store.dispatch({ type: 'INCREASE' })
-store.dispatch({ type: 'INCREASE' })
-
-
 
 class App extends Component {
+
   render() {
+    const { Increase, Decrease } = this.props;
     return (
       <div className="App">
-        <p>num:</p>
+        <p>num:{this.props.num}</p>
         <div>
-          <button>increase</button>
+          <button onClick={Increase}>increase</button>
           <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <button>decrease</button>
+          <button onClick={Decrease}>decrease</button>
         </div>
       </div>
     );
--- redux_demo2/src/index.js
+++ redux_demo2/src/index.js
@@ -4,5 +4,60 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
+
+let num = 0;
+
+// 定义reducer(计算规则)
+let reducer = (state = num, action) => {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DECREASE":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+// 根据计算规则，生成store
+let store = createStore(reducer)
+
+// 需要渲染什么数据
+let mapStateToProps = (state) => {
+  return {
+    num: state
+  }
+}
+
+// 触发数据变化
+let mapDispatchToProps = (dispatch) => {
+  return {
+    Increase: () => {
+      store.dispatch({ type: 'INCREASE' })
+    },
+    Decrease: () => {
+      store.dispatch({ type: 'DECREASE' })
+    }
+  }
+}
+
+// 派发规则
+// store.subscribe(() => {
+//   console.log(`current state:${store.getState()}`)
+// })
+
+// 触发数据变化
+// store.dispatch({ type: 'INCREASE' })
+// store.dispatch({ type: 'INCREASE' })
+
+//连接组件
+let MyApp = connect(mapStateToProps, mapDispatchToProps)(App)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <MyApp />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
