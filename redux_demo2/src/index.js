eeb1ad0103fbdf4e10e02ccc7516effb64280712
srect <fcqun@sina.cn>
2018-07-31 13:22:08 +0800

    :construction: 调整目录

--- redux_demo2/src/index.js
+++ redux_demo2/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/Home/App';
 import registerServiceWorker from './registerServiceWorker';
 
 // import { createStore } from 'redux'
