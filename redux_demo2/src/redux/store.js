ceee413123962994ae9574d62e99ed4accca21f5
srect <fcqun@sina.cn>
2018-07-31 13:18:33 +0800

    :sparkles: 使用redux工具调试

--- redux_demo2/src/redux/store.js
+++ redux_demo2/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore } from 'redux'
-
+import { composeWithDevTools } from 'redux-devtools-extension'; // 使用Redux工具调试
 import rootReducer from './reducer'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
