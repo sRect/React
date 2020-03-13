6da9ecef193390936fd78e7b6dbba9d5dfad950d
fangchaoqun <fcqun@sina.cn>
2020-03-13 17:01:41 +0800

    feat: :sparkles: useMemo

--- react_hooks/src/components/useMemo/index.js
+++ react_hooks/src/components/useMemo/index.js
@@ -11,17 +11,22 @@ const MyUseMemoChild = memo(({obj}) => {
 const MyUseMemo = () => {
   console.log("MyUseMemo render ==>");
   const [num, setNum] = useState(100);
-  const name = "hello"
+  const name = "hello";
 
+  // 当我们点击按钮更新count的时候，Effect组件会render，一旦render，回重新执行下面这行代码
+  // 这一行代码会生成有新的内存地址的对象，那么就算带着memo的Child组件，也会跟着重新render
+  // 尽管最后其实Child使用到的值没有改变
   // const obj = {
-  //   num
+  //   name
   // }
 
+  // useMemo有暂存功能，会将name值与上一次进行对比
+  // 如果name没有改变，name这次的obj就不重新赋值生成新的对象了
   const obj = useMemo(() => {
     return {
       name
     }
-  }, [name])
+  }, [name]);
 
   return (
     <div>
